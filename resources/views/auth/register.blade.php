a115367ca81e874d17ecd643bcbc6f06ee4f0f5c
2151571@my.ipleiria.pt <14865635>
2018-05-07 19:27:06 +0100

    Alteraçoes dos parametros do phone (optional)

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -65,7 +65,7 @@
                             <label for="phone" class="col-md-4 col-form-label text-md-right">{{ __('Phone Number') }}</label>
 
                             <div class="col-md-6">
-                                <input id="phone" type="text" class="form-control" name="phone" required>
+                                <input id="phone" type="text" class="form-control" name="phone" optional>
                             </div>
                         </div>
 
