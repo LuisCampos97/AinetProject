f165a82f7c731c3fc52946efa913d4ee0702d26b
2151571@my.ipleiria.pt <14865635>
2018-05-07 20:17:12 +0100

    Alteração de erros do phonenumber

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -51,8 +51,8 @@ class RegisterController extends Controller
         return Validator::make($data, [
             'name' => 'required|string|max:255',
             'email' => 'required|string|email|max:255|unique:users',
-            'password' => 'required|string|min:3|confirmed',
-            'phone' =>'optional|min:6'
+            'password' => 'required|string|min:3|confirmed'
+            
         ]);
     }
 
