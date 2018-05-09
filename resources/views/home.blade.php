26f420a75fff814b651f5bd70238f69f988d2193
sergiorico3 <clubofsk8ers@hotmail.com>
2018-05-09 16:41:01 +0100

    wtf

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -11,20 +11,13 @@
                         <div class="alert alert-success">
                             {{ session('status') }}
                         </div>
-<<<<<<< HEAD
-                    @endif                    You are logged in!
-=======
                     @endif
                     You are logged in!
->>>>>>> b663ecb1834b2ac31387f898ea7c0d9625af6f7a
                 </div>
             </div>
-<<<<<<< HEAD
             <div class="card">
                 <div class="card-body">
                     <a class="card-body" href="http://ainetproject.ainet/users">List of registered users</a>
                 <a href="{{ url('/users') }}">User's List</a>
-=======
->>>>>>> b663ecb1834b2ac31387f898ea7c0d9625af6f7a
         </div>
     </div>
