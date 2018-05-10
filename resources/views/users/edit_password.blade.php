661ca0480f911c94a692d03fce15c19586a567e6
2151571@my.ipleiria.pt <14865635>
2018-05-10 20:02:41 +0100

    Alteração do nome no botao para editar a pass

--- resources/views/users/edit_password.blade.php
+++ resources/views/users/edit_password.blade.php
@@ -16,7 +16,7 @@
                             <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Current Password') }}</label>
 
                             <div class="col-md-6">
-                                <input id="password" type="password" class="form-control" name="password" required>   
+                                <input id="old_password" type="password" class="form-control" name="old_password" required>   
                             </div>
                         </div>
 
@@ -38,7 +38,7 @@
                             <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Confirm Password') }}</label>
 
                             <div class="col-md-6">
-                                <input id="password" type="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password_confirmation" required>
+                                <input id="password_confirmation" type="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password_confirmation" required>
                                 @if ($errors->has('password'))
                                     <span class="invalid-feedback">
                                         <strong>{{ $errors->first('password') }}</strong>
@@ -50,7 +50,7 @@
                         <div class="form-group row mb-0">
                             <div class="col-md-6 offset-md-4">
                                 <button type="submit" class="btn btn-primary">
-                                    {{ __('Reset Password') }}
+                                    {{ __('Update Password') }}
                                 </button>
                             </div>
                         </div>
