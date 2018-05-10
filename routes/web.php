b93ae844772d4a29b3b0f37d5ce91ccec1c7a15a
sergiorico3 <clubofsk8ers@hotmail.com>
2018-05-10 17:39:00 +0100

    searchcontroller

--- routes/web.php
+++ routes/web.php
@@ -27,10 +27,8 @@ Route::get('/home', 'HomeController@index')->name('home');
 Route::get('/me/profile', 'UserController@edit')->name('edit');
 Route::put('/home', 'UserController@update')->name('update');
 
-<<<<<<< HEAD
-//List users
-Route::get('/', 'SearchController@index')->name('search');
-=======
 Route::post('/me/password', 'UserController@editPassword')->name('editPassword');
 Route::patch('/home', 'UserController@updatePassword')->name('updatePassword');
->>>>>>> f33f518b17c0cc903c8cfb35ba42eb5ffe84a27f
+
+//List users
+Route::get('/', 'SearchController@index')->name('search');
