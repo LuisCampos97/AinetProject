2b1bbb8ebcea875f38b9ea3ea092b5d4c8703927
Luís Campos <33434500+LuisCampos97@users.noreply.github.com>
2018-05-10 18:09:43 +0100

    Conices

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,6 +1,4 @@
-@extends('layouts.app')
-
-@section('content')
+@extends('layouts.app') @section('content')
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
@@ -8,17 +6,22 @@
                 <div class="card-header">Dashboard</div>
                 <div class="card-body">
                     @if (session('status'))
-                        <div class="alert alert-success">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-                    You are logged in!<br><br>
-                    <img src="/profiles/{{ Auth::user()->profile_photo }}" alt="Imagem">
-                    <strong>User Name: </strong>{{ Auth::user()->name }}<br>
-                    <strong>Email: </strong>{{ Auth::user()->email }} <br>
-                    <strong>Phone Number: </strong> {{ Auth::user()->phone }}
+                    <div class="alert alert-success">
+                        {{ session('status') }}
+                    </div>
+                    @endif You are logged in!
+                    <br>
+                    <br>
+                    <img src="/profiles/{{ Auth::user()->profile_photo }}" class="rounded float-left" alt="Imagem" style="padding-right: 10px">
+                    <div style="font-size: 20px">
+                        <strong>User Name: </strong>{{ Auth::user()->name }}
+                        <br>
+                        <strong>Email: </strong>{{ Auth::user()->email }}
+                        <br>
+                        <strong>Phone Number: </strong> {{ Auth::user()->phone }}
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-@endsection
+    @endsection
